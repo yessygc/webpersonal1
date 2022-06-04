b03d5bceb172c5885d2dcd91ca03a970f21c9970
yessygc <yessgarciacampos@gmail.com>
2022-06-03 20:55:51 -0500

    commit 2, desde creando cabecera dinamica hasta el final del proyecto

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project  
 
 # Create your views here.
+def portafolio(request):
+    projects = Project.objects.all()  
+    return render(request, "portafolio/portafolio.html",
+        {'projects':projects})
